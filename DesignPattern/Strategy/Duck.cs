8c3e73f261520a955999f074aa4561ceadfefeb8
fan yan <1844609673@qq.com>
2022-04-15 21:37:14 +0800

    Created

--- DesignPattern/Strategy/Duck.cs
+++ DesignPattern/Strategy/Duck.cs
@@ -8,9 +8,42 @@ namespace DesignPattern.Strategy
 {
     public class Duck
     {
+        public IFlyBehavior FlyBehavior { get; set; }
+        public IQuackBehavior QuackBehavior { get; set; }
+
+        public Duck()
+        {
+        }
+
+        public void PerformFly()
+        {
+            FlyBehavior.Fly();
+        }
+
+        public void PerformQuack()
+        {
+            QuackBehavior.Quack();
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("All ducks float, even decoys!");
+        }
+
         public void Display()
         {
-
+            Console.WriteLine("I'm a real duck.");
         }
+
+        public void SetFlyBehavior(IFlyBehavior flyBehavior)
+        {
+            FlyBehavior = flyBehavior;
+        }
+
+        public void SetQuackBehavior(IQuackBehavior quackBehavior)
+        {
+            QuackBehavior = quackBehavior;
+        }
+        
     }
 }
